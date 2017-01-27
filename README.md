d8b66900107ad032b3e37818a7a1f4d44fb105ae
Sung Kim <hunkim@gmail.com>
2017-01-27 23:56:04 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Top Deep Learning Projects
+A list of popular github projects related to deep learning (ranked by stars automatically).
+Please update list.txt (via pull requests)
+
+| Project Name| Stars | Description |
+| ------- | ------ | ------ |
+| [tensorflow](https://github.com/tensorflow/tensorflow) | 44100 | Computation using data flow graphs for scalable machine learning
+| [caffe](https://github.com/BVLC/caffe) | 15605 | Caffe: a fast open framework for deep learning.
+| [neural-style](https://github.com/jcjohnson/neural-style) | 12490 | Torch implementation of neural style algorithm
+| [keras](https://github.com/fchollet/keras) | 11614 | Deep Learning library for Python. Convnets, recurrent neural networks, and more. Runs on Theano or TensorFlow.
+| [deepdream](https://github.com/google/deepdream) | 9757 | 
+| [RocAlphaGo](https://github.com/Rochester-NRT/RocAlphaGo) | 7806 | An independent, student-led replication of DeepMind's 2016 Nature publication, "Mastering the game of Go with deep neural networks and tree search" (Nature 529, 484-489, 28 Jan 2016), details of which can be found on their website https://deepmind.com/publications.html.
+| [TopDeepLearning](https://github.com/aymericdamien/TopDeepLearning) | 3556 | A list of popular github projects related to deep learning
+| [TensorFlow-Tutorials](https://github.com/nlintz/TensorFlow-Tutorials) | 3111 | Simple tutorials using Google's TensorFlow Framework
+
+Inspired by https://github.com/aymericdamien/TopDeepLearning
---
+++ list2md.py
@@ -0,0 +1,50 @@
+import cgitb
+import requests
+import config as cf
+
+cgitb.enable(format='text')
+
+result=[]
+
+head = '# Top Deep Learning Projects\n' \
+        'A list of popular github projects related to deep learning (ranked by stars automatically).\n'\
+        'Please update list.txt (via pull requests)\n\n' \
+        '| Project Name| Stars | Description |\n' \
+        '| ------- | ------ | ------ |\n'
+
+tail = '\nInspired by https://github.com/aymericdamien/TopDeepLearning'
+
+with open("list.txt") as f:
+    list = f.readlines()
+    for url in list:
+        if url.startswith("https://github.com/"):
+            api_url = "https://api.github.com/repos/" + url[19:].strip().strip("/")
+            api_url = api_url + '?access_token=' + cf.ACCESS_TOKEN
+            print(api_url)
+            r = requests.get(url=api_url)
+            r_json = r.json()
+            if 'name' in r_json:
+                result.append({'name': r_json['name'],
+                               'description': r_json['description'],
+                               'forks': r_json['forks_count'],
+                               'created': r_json['created_at'],
+                               'updated': r_json['updated_at'],
+                               'url': r_json['html_url'],
+                               'stars': r_json['stargazers_count']})
+
+
+sorted_result = sorted(result, key=lambda repos: repos['stars'], reverse=True)
+print(sorted_result)
+
+def xstr(s):
+    if s is None:
+        return ''
+    return str(s)
+
+with open("README.md", 'w') as out:
+    out.write(head)
+    for repos in sorted_result:
+        out.write('| [' +
+                  repos['name'] + '](' + repos['url']  + ') | ' +
+                  xstr(repos['stars']) + ' | ' + xstr(repos['description']) + '\n')
+    out.write(tail)
